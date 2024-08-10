91fd80ee14fce76ab7af55126934a4c1cbe758a2
Yogesh-C21 <jatyogesh1999@gmail.com>
2024-08-10 17:41:34 +0530

    Book Store, Admin and Model Relationships

--- blog_app/settings.py
+++ blog_app/settings.py
@@ -10,7 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,6 +36,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'book_store',
+    'blog',
+    'crud_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +62,9 @@ ROOT_URLCONF = 'blog_app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / "templates"
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -73,10 +83,21 @@ WSGI_APPLICATION = 'blog_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT', '3306'),
     }
 }
 
---
+++ crud_app/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponse
+
+def item_list(request):
+    return HttpResponse('Balle Balle')
+    # items = Item.objects.all()
+    # return render(request, 'item_list.html', {'items': items})
+
+# def item_detail(request, pk):
+#     item = get_object_or_404(Item, pk=pk)
+#     return render(request, 'item_detail.html', {'item': item})
+
+# def item_create(request):
+#     if request.method == "POST":
+#         form = ItemForm(request.POST)
+#         if form.is_valid():
+#             item = form.save()
+#             return redirect('item_detail', pk=item.pk)
+#     else:
+#         form = ItemForm()
+#     return render(request, 'item_form.html', {'form': form})
+
+# def item_update(request, pk):
+#     item = get_object_or_404(Item, pk=pk)
+#     if request.method == "POST":
+#         form = ItemForm(request.POST, instance=item)
+#         if form.is_valid():
+#             item = form.save()
+#             return redirect('item_detail', pk=item.pk)
+#     else:
+#         form = ItemForm(instance=item)
+#     return render(request, 'item_form.html', {'form': form})
+
+# def item_delete(request, pk):
+#     item = get_object_or_404(Item, pk=pk)
+#     if request.method == "POST":
+#         item.delete()
+#         return redirect('item_list')
+#     return render(request, 'item_confirm_delete.html', {'item': item})
